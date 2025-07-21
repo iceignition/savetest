9159ca5f3692549b066e820dc60f203fdc6e5f47
Jaemin An <jaemin9945@mail.hongik.ac.kr>
2025-07-21 15:15:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,49 +10,41 @@ window.addEventListener('scroll', () => {
   if (scrollPercent < 1 / 6) {
     headline.innerHTML = `
       2025<br>
-      Creative Direction for 
-      <a href="./cv/cv.html" style={{textDecoration: 'underline',color: 'inherit',fontWeight: 'inherit'}}>'Accumulated Hits'</a><br>
-      Visual direction, Exhibition planning, Digital content production,<br>
-      Graphic design, VFX
+      Creative Direction for 'Accumulated Hits'<br>
+      Visual Direction, Exhibition Planning, Digital Content Production,<br>
+      Graphic Design, VFX
     `;
   } else if (scrollPercent < 3 / 6) {
     headline.innerHTML = `
       2025 <br>
-      Creative Direction for Graphic Performance Video,'Chiaroscuro' <br>
-      Filming, Editing, VFX, Graphic Design, <br>
+      Creative Direction for the Graphic Performance Video, 'Chiaroscuro' <br>
+      Filming, Editing, VFX, Graphic Design,<br>
       Book Design, Writing, Installation Design
-
     `;
   } else if (scrollPercent < 4 / 6) {
     headline.innerHTML = `
       2024<br>
-      Brand Visual Direction for 'kyolameth' <br>
-      Creative Direction, Brand Identity Design, 3D Graphic Design <br>
+      Brand Visual Direction for 'Kyolameth' <br>
+      Creative Direction, Brand Identity Design, 3D Graphic Design,<br>
       AI Image Generation
     `;
   } else if (scrollPercent < 4.8 / 6) {
     headline.innerHTML = `
       2024<br>
-      Creative Direction for 'SSGI'<br>
-      Brand Identity Design, Content Creation, Editorial
+      Creative Direction for 'SSGI' Magazine <br>
+      Brand Identity Design, Content Creation, Editorial Design
     `;
   } else if (scrollPercent < 5.4 / 6) {
     headline.innerHTML = `
       2025 <br>
       Visual Direction for 'Kuai Kuai' <br>
-      SNS Contents Creation, Photography, Editing
+      Social Media Content Creation, Photography, Editing
     `;
-  }else if (scrollPercent < 6 / 6) {
+  } else if (scrollPercent < 6 / 6) {
     headline.innerHTML = `
       2024 <br>
-      Exhibition Design for 'Time Spectrum' <br>
-      Exhibition Identity Design Pster Design, Reaflet Design, 
-    `;
-  }else {
-    headline.innerHTML = `
-      Jaemin An explores the overflow of visual culture.<br>
-      He now experiments with <a href="./cv/cv.html">surface, light, and rhythm</a><br>
-      to disturb the linear reading of images.
+      Virtual Exhibition Design for 'Time Spectrum' <br>
+      Exhibition Identity Design, Poster Design, Leaflet Design
     `;
   }
 });
